2eecbae3687fda54f18ff89b51985cb398602c71
Rishi Raj <rishiraj88+github@gmail.com>
2026-05-18 08:57:51 +0200

    Create two simple program listings to teach to beginners.

---
+++ 02_comments/end/main.rs
@@ -0,0 +1,6 @@
+// one-line comment (more popular in modern era as well as the only option in 1970s.)
+fn main() {
+    /* multiple-line comment.
+       Enjoyed a lot of traction in the mid-era of programming with computing devices */
+    println!("Hello, world!");
+}
